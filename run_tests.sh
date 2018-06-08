e4101e3a95e6ea10cb586a40bd629d942d798089
mwas <francismwangi152@gmail.com>
2018-06-08 12:48:28 +0300

    running tests in parallel.

--- run_tests.sh
+++ run_tests.sh
@@ -2,34 +2,29 @@
 
 set -e
 
-python_tags=('3.6.5-alpine3.7' '2.7.15-alpine3.7')
-djangoVersions=('2.0.6' '1.11.1')
+# Pass the first argument as python tag and second argument as django version
+python_version=${1:-'3.6.5-alpine3.7'}
+django_version=${2:-'2.0.6'}
 supported_db_host=(scylla cassandra)
 
-# test different python versions
-for python_version in "${python_tags[@]}"
-do
-  # test django versions
-  for django_version in "${djangoVersions[@]}"
-  do
-    echo "Testing with"
-    echo "python version: ${python_version}"
-    echo "django version: ${django_version}"
 
+echo "Testing with"
+echo "python version: ${python_version}"
+echo "django version: ${django_version}"
 
-    if [[ ${python_version} =~ ^2. ]] && [[ ${django_version} =~ ^2. ]]; then
-        echo "django version 2 not supported in python 3 skipping this test"
-    else
-        docker-compose build \
-        --build-arg PYTHON_IMAGE_TAG=${python_version} \
-        --build-arg DJANGO_VERSION=${django_version} app
+# '2.7.15-alpine3.7'
+# '1.11.1'
+if [[ ${python_version} =~ ^2. ]] && [[ ${django_version} =~ ^2. ]]; then
+    echo "django version 2 not supported in python 3 skipping this test"
+else
+    docker-compose build \
+    --build-arg PYTHON_IMAGE_TAG=${python_version} \
+    --build-arg DJANGO_VERSION=${django_version} app
 
-        for host in "${supported_db_host[@]}"
-        do
-            sudo find . -name \*.pyc -delete
-            echo "db host: ${host}"
-            docker-compose run -e SCYLLA_HOST=${host} app python manage.py test
-        done
-    fi
-  done
-done
+    for host in "${supported_db_host[@]}"
+    do
+        sudo find . -name \*.pyc -delete
+        echo "db host: ${host}"
+        docker-compose run -e SCYLLA_HOST=${host} app python manage.py test
+    done
+fi
