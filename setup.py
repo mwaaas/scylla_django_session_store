c2cacf930925efcfcc70281f36bf0ec5384a9ae4
Francis Mwangi <francismwangi152@gmail.com>
2018-06-11 13:05:05 +0300

    Feaure/adding command to delete all sessions (#4)
    * version bump up.
    
    * adding command to delete all sessions from a table
    
    * version bumpup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scylla_django_session_store',
-    version='0.0.2',
+    version='0.0.4',
     packages=['tests', 'scylla_sessions', 'scylla_sessions.management',
               'scylla_sessions.management.commands'],
     url='https://github.com/mwaaas/scylla_django_session_store',
